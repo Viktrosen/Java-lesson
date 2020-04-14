4c8a1bf81c355368567d8b8f7376baf97c97c7e2
Viktrosen <bolotov.vit@yandex.ru>
2020-04-14 19:24:41 +0500

    Создал все 3 класса

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,6 @@
 public class MainClass {
     public static void main(String[] args) {
-        Course c = new Course(); // Создаем полосу препятствий
-        Team team = new Team(); // Создаем команду
-        c.doIt(team); // Просим команду пройти полосу
-        team.showResults(); // Показываем результаты
+
     }
 
 }
